1984e8a891d7fb19acb4275f1c978e27dd015a4c
MoatazFarid <MoatazMFarid@gmail.com>
2016-10-13 13:47:10 +0200

    gg

---
+++ registeration/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class RegisterationConfig (AppConfig):
+	name ='registeration'
---
+++ registeration/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from . import views
+
+from registeration.views import *
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^logout_user/$', views.logout_user, name='logout_user'),
+]
