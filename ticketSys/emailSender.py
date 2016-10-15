ec0a2e0f9d7056f399a54e312abb5cedce1c70e2
Moataz Farid <moatazmfarid@gmail.com>
2016-10-15 02:07:41 +0200

    team modifications

---
+++ ticketSys/emailSender.py
@@ -0,0 +1,87 @@
+# -*- coding: cp1252 -*-
+## ASU Racing Team IT Department
+## Automated E-mail Sender
+## Created 24/9/2016
+## Use Python shell
+## "NOTE THIS PROGRAM SENDS ONLY EMAILS FROM GMAIL" argument1->TextFile  argument2->E-mail
+##//  python Script.py emails.txt example@gmail.com  // and then enter
+
+####NOTE "USE THE RIGHT FORMAT FOR THE E-MAILS SHEET"
+##recipantName
+##E-mailAddress
+
+import getpass
+import sys
+import re
+import os
+import smtplib  ##liberary that will be used to send mails to internet machines
+from smtplib import SMTPException
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# E-mail address used for sending mails, ##'WILL BE SEEN BY THE RECIPANT'
+fromAddress = 'testasurtit@gmail.com'  ##Enter a sender MAIL ADDRESS as a SECOND ARGUMENT
+
+username = fromAddress  # User name used for logging to the Gmail server
+password = 'testasurtit1'  # The password related to the username
+massage = ' '  # massage will be sent by the system and will be modefied for each recipant
+
+
+##A function sets up the protocole object and sends e-mailes to the server
+def autoMailSender(receiver):
+    toAddress = receiver  # E-mail address of the recipant, the example mail will be replaced by different addresses from the mailes file
+    def sendEmail():
+        server = smtplib.SMTP('smtp.gmail.com:587')  # Using Gmail
+        server.ehlo()  # Our System's object and the server shaking hand
+        server.starttls()  # Something to do with encryption
+        server.login(username, password)  # Logging to the server
+        server.sendmail(fromAddress, toAddress, massage)  # Sending e-mail using the information above
+        server.quit()  # Stop Communication with the server
+
+
+
+    try:
+
+        ##Loop throw each line
+
+        try:
+            msg = MIMEMultipart('alternative')  # Choose alternative for the mail extension to mix between html and text
+            msg['Subject'] = "FSUK'17 job vacancy application"  # Set the Subject this method will set it automaticaly to the header
+            msg['From'] = 'HR Department ASURT'  # The From attribute will show in the lable of the E-mail
+            msg['To'] = toAddress  # The To attribute will appear to the recipant
+
+            # Creating the TEXT and the HTML part of the massage
+            text = "Hi this is an Automated mail from ASURT send to"
+            html = """
+            <html>
+              <head></head>
+              <body>
+                <p>
+                   check the ticket System
+                   Best regards,<br>
+                   HR-Department,<br>
+                   ASU Racing Team.<br>
+                </p>
+              </body>
+            </html>
+            """
+            # Define the type of each part
+            part1 = MIMEText(text, 'plain')
+            part2 = MIMEText(html, 'html')
+            msg.attach(part1)
+            msg.attach(part2)
+            massage = msg.as_string()
+            # FINISH Creating massage to send
+            sendEmail()
+        except:
+            print("---ERROR---\nHas not been sent", toAddress)
+
+
+    except smtplib.SMTPException:
+        print ("Error: unable to send email ,\n try Access for less secure apps \nfor GMAIL go to:\nhttps://www.google.com/settings/security/lesssecureapps")
+        sys.exit("Error")
+
+
+
+
+
--- ticketSys/views.py
+++ ticketSys/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Ticket
+# from .models import Ticket
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from rtMembers.models import RTMember
+from rtMembers.models import RTMember, Roles
 from ticketSys.models import TicketMember, TicketComment, Ticket, States
 from django.contrib.auth.models import User
+from django.db.models import Q
+from ticketSys import emailSender
 
 
 def detail(request, pk):
@@ -17,7 +19,7 @@ def detail(request, pk):
 def addComment(request, pk):
     comment = request.POST['comment']
     user = request.user
-    tm = getTicketMember(user)
+    tm = getTicketMemberFromUser(user)
     ticket = Ticket.objects.get(id=pk)
     comm = TicketComment(memberid=tm, ticketid=ticket, comment=comment)
     comm.save()
@@ -29,33 +31,75 @@ def changeState(request, pk):
     t = Ticket.objects.get(id=pk)
     # check if the member able to change the state of the ticket
     # if state was  'waiting_it'
-    # the IT members  or All heads only can be able to change the state
+    # the IT members or All heads only can be able to change the state
     # NOTE any Managerial head begin with X
-    #(0, 'WAITING_HR'), (1, 'WAITING_IT'), (2, 'WAITING'), (3, 'SOLVED')
-    if t.state == 1:
-        print ('plzpzlz')
-        # check loged must be IT members or all heads
-        member = get_object_or_404(RTMember,pk=request.user) # thats the logged in member in RT table
-        if (member.role[0]=='X')or (member.role=='IT') :
-            print ('plapla')
-            #if t.state is not s:
-            # stateChanged(s) #to notify who should be notified by email or whatever
-            t.state = s
-            t.save()
-
-
+    # (0, 'WAITING_HR'), (1, 'WAITING_IT'), (2, 'WAITING'), (3, 'SOLVED')
+    if t.state is not s:
+        emailRelatedMembers(s, pk)  # to notify who should be notified by email or whatever
+        t.state = s
+        t.save()
     return redirect('ticketSys:detail', pk)
 
 
+# todo don't show RTMembers in the user list
+# todo can search for the user
 class TicketCreate(CreateView):
     model = Ticket
-    fields = ['userid', 'state', 'title', 'content']  # member id should be taken from the logged in user
+    fields = ['userid', 'title', 'content']  # member id should be taken from the logged in user
 
     def form_valid(self, form):
         ticket = form.save(commit=False)
+        try:
+            self.request.POST['needhr']
+            # Need HR approval
+            ticket.state = 0
+            emailHR()
+        except:  # Does not need HR approval
+            ticket.state = 1
+            emailIT()
+
         user = self.request.user
-        ticket.memberid = getTicketMember(user)
+        try:
+            ticket.memberid = getTicketMemberFromUser(user)
+        except:
+            return redirect('ticketSys:dashBoard')
         return super(TicketCreate, self).form_valid(form)
+
+
+# todo only send emails for members in the TicketMembers
+def emailHR():
+    rtMembers = RTMember.objects.filter(
+        Q(role=2) | Q(role=3) | Q(role=4) | Q(role=5))  # the indices of the HR(OD,REC) in ROLES
+    for rtMember in rtMembers:
+        receiver = getUserFromRTMember(rtMember)
+        if receiver.email is not None:
+            emailSender.autoMailSender(receiver.email)
+
+
+# todo only send emails for members in the TicketMembers
+def emailIT():
+    rtMembers = RTMember.objects.filter(Q(role=0) | Q(role=1))  # the indices of the IT in ROLES
+    for rtMember in rtMembers:
+        receiver = getUserFromRTMember(rtMember)
+        if receiver.email is not None:
+            emailSender.autoMailSender(receiver.email)
+
+
+def emailRelatedMembers(state, pk):
+    # (0, 'WAITING_HR'), (1, 'WAITING_IT'), (2, 'WAITING'), (3, 'SOLVED')
+    if state == '0':  # handling state 0 -> HR
+        emailHR()
+
+    elif state == '1':  # handling state 1 -> IT
+        emailIT()
+
+    elif state == '2':  # send email to the ticket starter
+        ticket = Ticket.objects.get(id=pk)
+        ticketmember = TicketMember.objects.get(id=ticket.memberid_id)
+        rtMember = RTMember.objects.get(id=ticketmember.memberid_id)
+        receiver = getUserFromRTMember(rtMember)
+        if receiver.email is not None:
+            emailSender.autoMailSender(receiver.email)
 
 
 def dashBoard(request):
@@ -64,7 +108,11 @@ def dashBoard(request):
     return render(request, 'ticketSys/dashboard.html', context)
 
 
-def getTicketMember(user):
-    rtmember = RTMember.objects.get(userid=user)
-    ticketmember = TicketMember.objects.get(memberid=rtmember)
-    return ticketmember
+def getTicketMemberFromUser(user):  # returns the TicketMember associated with that User
+    rtmember = RTMember.objects.get(userid_id=user.id)
+    return TicketMember.objects.get(memberid_id=rtmember.id)
+
+
+def getUserFromRTMember(rtMember):  # returns the User associated with that RTMember
+    memberID = rtMember.userid.id
+    return User.objects.get(id=memberID)
