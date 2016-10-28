98879e71fcd0e26bda776ab3ff11a0c3d9df9974
Muhammad Azzazy <msa.azzazy@aucegypt.edu>
2016-10-28 19:23:21 +0200

    1st ajax test

--- ticketSys/views.py
+++ ticketSys/views.py
@@ -8,6 +8,12 @@ from ticketSys import emailSender
 from django.contrib.auth import authenticate, login
 from django.core.files.base import ContentFile
 
+# imports for testing Ajax
+import json
+from django.shortcuts import *
+from django.template import RequestContext
+from linki.forms import *
+
 
 # detail get called like this
 # detail(request=<HttpRequest object>, question_id='34')
@@ -220,3 +226,18 @@ def addRTMember(request):
     rtMember = RTMember(user=normalUser, role=roleOfRTMember)
     rtMember.save()
     return redirect('ticketSys:choose_RTMember')
+
+
+# def testAjax(request):
+#     if request.method == "POST":
+#         print('post recived')
+#
+#         message = 'something wrong!'
+#         if (form.is_valid()):
+#             print(request.POST['title'])
+#             message = request.POST['title']
+#
+#         return HttpResponse(json.dumps({'message': message}))
+#
+#     return render_to_response('contact/advert.html',
+#                               {'form': AdvertForm()}, RequestContext(request))
