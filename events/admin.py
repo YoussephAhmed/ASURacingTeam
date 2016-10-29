4ae537795716328c05a19d1156c3f49426c0e1b7
Moataz Farid <moatazmfarid@gmail.com>
2016-10-29 20:38:52 +0200

    adding events and clubs to django admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event,Reservation,Role,Organizer,Attendee
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Reservation)
+admin.site.register(Role)
+admin.site.register(Organizer)
+admin.site.register(Attendee)
--- foreignClubs/admin.py
+++ foreignClubs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Club,Head
 
-# Register your models here.
+admin.site.register(Head)
+admin.site.register(Club)
